4ed7cf520788fb15651bd09f8cf2f3b7143df7e8
MuAbCh <mabdullahc19@gmail.com>
2025-09-13 12:43:50 -0400

    arduino code for reading peripherals

---
+++ firmware/peripherals.ino
@@ -0,0 +1,28 @@
+//int x = A1; // don't need x 
+int y = A2;
+//int z = A3; // don't need z
+int tilt = A0;
+int pressure = A4;
+int buzzer = A1;
+
+void setup() {
+  Serial.begin(9600);
+  //pinMode(x, INPUT);
+  pinMode(y, INPUT);
+  //pinMode(z, INPUT);
+  pinMode(tilt, INPUT);
+  pinMode(pressure, INPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  //Serial.println(String(analogRead(x)) + "," + String(analogRead(y)) + "," + String(analogRead(z)));
+  Serial.println("Y: " + String(analogRead(y)));
+  Serial.println("Tilt: " + String(analogRead(tilt)));
+  Serial.println("Pressure: " + String(analogRead(pressure)));
+  tone(buzzer, 85);
+  tone(buzzer, 55);
+  tone(buzzer, 15);
+  noTone(buzzer);
+  delay(200); // only 200 for, will lower/ remove later
+}
